b44ffc99a99f24ec7174dc72f0f1711d56d976df
ebraheemabbas <ebraheemabbas51@gmail.com>
2019-12-15 12:59:36 +0200

    update the action page add icons style buttons having two buttons new print storage closes #13

--- src/components/Sensors-action-page/Sensors-actions.jsx
+++ src/components/Sensors-action-page/Sensors-actions.jsx
@@ -1,33 +1,36 @@
 import React from "react";
 import { useHistory } from "react-router-dom";
+import PrinterIcon from "../../Icons/printer.svg";
+import StorgeIcon from "../../Icons/database.svg";
+
 import "./Sensors-actions.css";
 export default function SensorsActionPage() {
   const history = useHistory();
   return (
     <React.Fragment>
       <div className="Container">
-        <div className="button-container">
-          <button
-            onClick={() => {
-              history.push("/new-print");
-            }}
-          >
-            Print new
-          </button>
-          <button
-            onClick={() => {
-              history.push("/Curing");
-            }}
-          >
-            Curing
-          </button>
-          <button
-            onClick={() => {
-              history.push("/Coating");
-            }}
-          >
-            Coating
-          </button>
+        <div className="buttons-container">
+          <div className="button-container">
+            <button
+              onClick={() => {
+                history.push("/new-print");
+              }}
+            >
+              <img src={PrinterIcon} alt="printer" />
+              <span>Print new</span>
+            </button>
+          </div>
+
+          <div className="button-container">
+            <button
+              onClick={() => {
+                history.push("/Sensors");
+              }}
+            >
+              <img src={StorgeIcon} alt="storage" />
+              <span>Storge</span>
+            </button>
+          </div>
         </div>
       </div>
     </React.Fragment>
