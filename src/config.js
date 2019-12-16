b3c00e6cd0904389ef6df19bd92da2f4cb8af66d
okarem <karem.omary@gmail.com>
2019-12-16 17:21:48 +0200

    fixed config.js file added production domain AGAIN! relates #30

--- src/config.js
+++ src/config.js
@@ -4,9 +4,7 @@ if (process.env.NODE_ENV == "development") {
   endpointUrl = `http://localhost:${port}`;
 }
 if (process.env.NODE_ENV == "production") {
-  // TODO: change this endpoint for production, it shouldn"t be localhost:4000
     endpointUrl = `http://ec2-3-122-193-228.eu-central-1.compute.amazonaws.com:${port}`;
-  throw new Error("you need to set production sever url here");
 }
 
 export default endpointUrl;
