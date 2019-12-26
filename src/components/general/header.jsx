5b01e5a82962c9b3536862d0796f9e4c7ba38950
ebraheemabbas <ebraheemabbas51@gmail.com>
2019-12-26 10:36:56 +0200

    adding sign out button add sign out button on 302 redirecte to land page closes #39

--- src/components/general/header.jsx
+++ src/components/general/header.jsx
@@ -3,21 +3,34 @@ import Clock from "react-live-clock";
 import checkAuth from "../../scripts/checkAuth";
 import { useAsync } from "react-async";
 import "./header.css";
+import endpointUrl from "../../config";
+import { useHistory } from "react-router-dom";
 
+const handleSignOut = history => {
+  fetch(`${endpointUrl}/signout`, {
+    credentials: "include"
+  }).then(res => (res.status === 302 ? (window.location = "/") : null));
+};
 export default function Header() {
   const { data, isPending } = useAsync({ promiseFn: checkAuth });
+  const history = useHistory();
 
   if (isPending) return "Loading...";
   return (
-    <React.Fragment>
-      <nav id="navbar">
-        <div className="nav-items">
+    <nav id="navbar">
+      <div className="nav-items">
+        <div className="auth">
           <Clock className="Clock" ticking={true} format={"L HH:mm"} />
-          <div className="userName">
-            {data && data.isAuthenticated ? data.employee : null}
-          </div>
+          <div>{data && data.isAuthenticated ? data.employee : null}</div>
         </div>
-      </nav>
-    </React.Fragment>
+        {data && data.isAuthenticated ? (
+          <div>
+            <button id="signout" onClick={() => handleSignOut(history)}>
+              sign out
+            </button>
+          </div>
+        ) : null}
+      </div>
+    </nav>
   );
 }
