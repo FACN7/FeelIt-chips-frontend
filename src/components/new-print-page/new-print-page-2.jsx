8df152b269c4ad2b4879c29ff8c9254f2f29a4a0
Jamie Coe <jamiecoe66@gmail.com>
2019-12-30 11:48:32 +0200

    fix additional merge conflict

--- src/components/new-print-page/new-print-page-2.jsx
+++ src/components/new-print-page/new-print-page-2.jsx
@@ -84,17 +84,8 @@ const NewPrintPage2 = () => {
               DONE
             </button>
           </div>
-<<<<<<< HEAD
         </div>      
     </tableContext.Provider>
-||||||| merged common ancestors
-        </div>
-      </React.Fragment>
-    </tableContext.Provider>
-=======
-        </div>
-      </tableContext.Provider>
->>>>>>> master
   );
 };
 
